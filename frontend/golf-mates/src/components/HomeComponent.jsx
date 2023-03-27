84321285b44c4103ad83a519508bacadf09d88ab
fregget <fbjuren@gmail.com>
2023-03-27 14:30:10 +0200

    Sign up working with problems

---
+++ frontend/golf-mates/src/components/HomeComponent.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import UserService from "../UserService";
+import { withRouter } from "../common/with-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class AddUserComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      location: "",
+      handicap: "",
+      hasCar: "",
+    };
+
+    this.changeUsernameHandler = this.changeUsernameHandler.bind(this);
+    this.changePasswordHandler = this.changePasswordHandler.bind(this);
+  }
+
+  componentDidMount() {
+    UserService.getUserDetails().then((response) => {
+      this.setState({ user: response.data });
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+            <div>
+              User Profile
+            </div>
+            <div>   
+            Username: {user.username}
+            Location: {user.location}
+            Handicap: {user.handicap}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(AddUserComponent);
--- src/main/java/golf/mates/demo/controllers/UserController.java
+++ src/main/java/golf/mates/demo/controllers/UserController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/users/")
 @RequiredArgsConstructor
 public class UserController {
