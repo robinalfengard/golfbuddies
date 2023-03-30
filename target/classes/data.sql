a27675b4d16b44817dc22a1d8b04b1cab87de430
Fredrik Bjurén <fbjuren@gmail.com>
2023-03-30 14:46:24 +0200

    Delete target/classes directory

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO 'User' (username, password, handicap, has_car) VALUES
-('Admin', 'password123', 25, TRUE);
-INSERT INTO 'User' (username, password, handicap, has_car) VALUES
-('Admin2', 'password1234', 10, FALSE);
